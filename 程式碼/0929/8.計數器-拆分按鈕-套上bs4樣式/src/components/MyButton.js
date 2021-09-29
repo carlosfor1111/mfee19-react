dd2a44d172ed3cfc6b32de881b7138d6f7b5340d
Eddy Chang <eddy@joomla.com.tw>
2021-09-29 15:02:48 +0800

    ad

---
+++ 程式碼/0929/8.計數器-拆分按鈕-套上bs4樣式/src/components/MyButton.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function MyButton(props) {
+  console.log(props)
+  const { text, value, setTotal, total } = props
+
+  return (
+    <>
+      <button
+        className="btn btn-secondary"
+        onClick={() => {
+          const newTotal = total + value < 1 ? 1 : total + value
+          setTotal(newTotal)
+        }}
+      >
+        {text}
+      </button>
+    </>
+  )
+}
+
+export default MyButton
