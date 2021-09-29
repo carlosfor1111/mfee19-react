03472794651a3aceae9143ee0a3112dd7830f984
Eddy Chang <eddy@joomla.com.tw>
2021-09-29 15:03:53 +0800

    Update MyButton.js

--- 程式碼/0929/8.計數器-拆分按鈕-套上bs4樣式/src/components/MyButton.js
+++ 程式碼/0929/8.計數器-拆分按鈕-套上bs4樣式/src/components/MyButton.js
@@ -9,6 +9,7 @@ function MyButton(props) {
       <button
         className="btn btn-secondary"
         onClick={() => {
+          // 限制最小數量為1
           const newTotal = total + value < 1 ? 1 : total + value
           setTotal(newTotal)
         }}
