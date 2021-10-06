4873e17a1e23ff8c170bed7c1ee76297710f8c9f
Eddy Chang <eddy@joomla.com.tw>
2021-10-06 10:32:48 +0800

    ad

---
+++ 程式碼/1006/5.生命周期-資料載入樣式-spinner動畫/components/ProductFC.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+
+// 產品訂購的項目
+const productsFromServer = [
+  {
+    id: 1,
+    name: '咖啡色 T-shirt',
+    category: 'Shirt',
+    image: 'https://i.imgur.com/1GrakTl.jpg',
+    price: 300,
+  },
+  {
+    id: 2,
+    name: '白色 T-shirt',
+    category: 'Shirt',
+    image: 'https://i.imgur.com/ba3tvGm.jpg',
+    price: 200,
+  },
+  {
+    id: 3,
+    name: '黑色 T-shirt',
+    category: 'Shirt',
+    image: 'https://i.imgur.com/pHQ3xT3.jpg',
+    price: 450,
+  },
+]
+
+function ProductFC(props) {
+  // [{
+  //   id: 1,
+  //   name: '咖啡色 T-shirt',
+  //   category: 'Shirt',
+  //   image: 'https://i.imgur.com/1GrakTl.jpg',
+  //   price: 300,
+  // }]
+  const [products, setProducts] = useState([])
+  // 為了要切換載入動畫提示
+  const [isLoading, setIsLoading] = useState(true)
+
+  // didMount
+  // 第二個傳入參數為空陣列時，只執行第一次(讀取到時)
+  useEffect(() => {
+    // 模擬商品從伺服器來(fetch/ajax)
+    setTimeout(() => {
+      setProducts(productsFromServer)
+    }, 1000)
+
+    // 把載入動畫提示關起來
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  }, [])
+
+  const spinner = (
+    <div className="spinner-border text-primary" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  )
+
+  const display = (
+    <ul>
+      {products.map((v, i) => {
+        return <li key={i}>{v.name}</li>
+      })}
+    </ul>
+  )
+
+  return (
+    <>
+      <h1>商品資料</h1>
+      {/* 用載入指示的旗標來切換呈現 */}
+      {isLoading ? spinner : display}
+    </>
+  )
+}
+
+export default ProductFC
